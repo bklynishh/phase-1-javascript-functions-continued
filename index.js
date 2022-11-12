125b2fbcb3b5c58764de52ae58b7f3024cb0e2d7
Hannah Maratea <maratea101@gmail.com>
2022-11-12 14:00:32 -0500

    commit tree 545093f4705c6d19bccc6c269d53077ff7c188cc

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// saturdayFun
+function saturdayFun(transportation = 'roller-skate') {
+    return `This Saturday, I want to ${transportation}!`
+}
+
+// mondayWork
+function mondayWork (action = 'go to the office') {
+    return `This Monday, I will ${action}.`
+}
+
+// wrapAdjective
+// function wrapAdjective(result = '*', emphatic = 'a hard worker') {
+//     return `You are a ${result}${emphatic}${result}!`
+// }
+function wrapAdjective (paramenter2 = '*') {
+    return function name(parameter1 = 'special') {
+        return `You are ${paramenter2}${parameter1}${paramenter2}!`
+      }
+}
